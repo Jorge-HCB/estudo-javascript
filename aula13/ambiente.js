85bae8de666065dcd394b7165267ab31946f5ca4
Jorge Cavalcante <jhenriquebarreto150@gmail.com>
2023-05-25 22:04:58 -0300

    estudo javascript

---
+++ aula13/ambiente.js
@@ -0,0 +1,16 @@
+var c = 1
+do
+{
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 60)
+
+/*
+    while (c <= 60)
+    {
+        console.log(`Passo ${c}`)
+        c++
+    }
+*/
+
+//console.log('Tudo bem?')
